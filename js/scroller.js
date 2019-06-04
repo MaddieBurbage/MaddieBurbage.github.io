f9e5e2461ae6183131601422db36697930ceb575
MaddieBurbage <madeline.g.burbage@gmail.com>
2019-06-03 23:37:38 -0700

    added Collision project

--- js/scroller.js
+++ js/scroller.js
@@ -1,12 +1,20 @@
 window.onscroll = function() {
   var heading = document.getElementsByTagName("header")[0].firstElementChild;
   if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    heading.style.cssFloat = "left";
     heading.innerHTML = "MB";
-    heading.style.margin = "10px";
+    heading.style.margin = "20px";
   } else {
-    heading.style.cssFloat = "initial";
     heading.innerHTML = "Maddie Burbage";
-    heading.style.margin = "50px";
+    heading.style.margin = "40px";
+    heading.style.marginLeft = "60px";
   }
 };
+
+window.onresize = resizeHeader;
+window.onload = resizeHeader;
+
+
+function resizeHeader() {
+    var headSize = document.getElementsByTagName("header")[0].clientHeight;
+    document.getElementsByClassName("container")[0].style.marginTop = headSize + "px";
+}
